b61faedeb21c588ec4bc29bf71245480166ed982
LauriKajakko <kajakko.lauri@gmail.com>
2020-09-03 11:44:16 +0300

    2.1
    done

---
+++ part2/Kurssitiedot/src/index.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const Course = ({course}) => {
+
+  return(
+    <div>
+    <h1>{course.name}</h1>
+      <ul>
+        {course.parts.map(part =>
+          <li key={part.id}>
+            {part.name} {part.exercises}
+          </li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Testiosa',
+        exercises: 420,
+        id: 666
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
