f81a89519b96a5ea19720fb64b12f45bb3676cc6
LauriKajakko <kajakko.lauri@gmail.com>
2020-09-09 19:02:28 +0300

    2.12
    done

---
+++ part2/maidentiedot/src/App.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios'
+
+const CountriesToShow = ({countries, newName}) => { 
+  let c = countries.filter(country => country.name.toLowerCase().includes(newName))
+  let n = c.length
+
+  if (n >= 10) {
+      return <p>too many countries</p>
+  } else if (n===1){
+    let country = c[0]
+    return(
+      <div>
+        <h1>{country.name}</h1>
+        <p>
+          capital {country.capital}
+        </p>
+        <p>
+          population {country.population}
+        </p>
+
+        <h3>languages</h3>
+        <ul>
+        {country.languages.map(language=>
+          <li key={language.name}>
+            {language.name}
+          </li>
+          )}
+        </ul>
+
+        <img src={country.flag} alt="flag" width="300" height="150"/>
+
+      </div>
+    )
+  } else {
+    return(
+      <ul>
+        {c.map(country=>
+          <li key={country.name}>
+            {country.name}
+          </li>
+          )}
+      </ul>
+      )
+    
+  }
+    
+    
+    
+}
+
+function App() {
+
+  const [countries, setCountries] = useState([])
+  const [ newName, setNewName ] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value) //keeps track of the text on the form
+  }
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        console.log('promise fulfilled')
+        setCountries(response.data)
+      })
+  }
+
+  useEffect(hook, [])
+
+
+
+  return (
+    <div >
+      find countries
+      <input 
+        value={newName}
+        onChange={handleNameChange}    
+      />
+      <CountriesToShow countries={countries} newName={newName}/>
+    </div>
+  );
+}
+
+export default App;
