5cc30a07f21791f0092b65a36ba9638623aab95c
LauriKajakko <kajakko.lauri@gmail.com>
2020-11-19 13:16:02 +0200

    5.14

--- part5/bloglist-frontend/src/tests/Blog.test.js
+++ part5/bloglist-frontend/src/tests/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from '../components/Blog'
 
 describe('<Blog />', () => {
@@ -30,6 +30,23 @@ describe('<Blog />', () => {
     const div = component.container.querySelector('.togglableContent')
 
     expect(div).toHaveStyle('display: none')
+  })
+
+  test('after clicking the button, something displayed', () => {
+    const button = component.getByText('info')
+    fireEvent.click(button)
+
+    const div = component.container.querySelector('.togglableContent')
+    expect(div).not.toHaveStyle('display: none')
+  })
+
+  test('after clicking the button, likes and url are displayed', () => {
+    const button = component.getByText('info')
+    fireEvent.click(button)
+
+    const div = component.container.querySelector('.togglableContent')
+    expect(div).toHaveTextContent('url:')
+    expect(div).toHaveTextContent('Likes:')
   })
 
 })
