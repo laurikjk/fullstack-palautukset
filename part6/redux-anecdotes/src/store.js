f62a4f68d6a3aaf0d3b439cb24445876cc2fc72c
LauriKajakko <kajakko.lauri@gmail.com>
2020-11-23 08:51:20 +0200

    6.13

--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -1,13 +1,18 @@
 
 import { createStore, combineReducers } from 'redux'
-import anecdoteReducer from './reducers/anecdoteReducer'
+import anecdoteReducer, { initAnecdotes } from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
+import anecdoteService from './services/anecdotes'
 
 const reducer = combineReducers({
-    anecdotes: anecdoteReducer,
-    notification: notificationReducer
+  anecdotes: anecdoteReducer,
+  notification: notificationReducer
 })
 
 const store = createStore(reducer)
+
+anecdoteService.getAll().then(anecdotes =>
+  store.dispatch(initAnecdotes(anecdotes))
+)
 
 export default store
