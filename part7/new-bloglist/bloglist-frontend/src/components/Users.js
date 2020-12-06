0ee296a89998873d64d2529ec240142687454dd2
LauriKajakko <kajakko.lauri@gmail.com>
2020-12-06 14:21:20 +0200

    got rid of unique key warning

--- part7/new-bloglist/bloglist-frontend/src/components/Users.js
+++ part7/new-bloglist/bloglist-frontend/src/components/Users.js
@@ -6,24 +6,33 @@ const Users = (props) => {
   return (
     <div>
     <h2> Users </h2>  
+    <table>
+      <tbody>
+        <tr>
+          <th>
+            user
+          </th>
+          <th>
+            blogs created
+          </th>
+        </tr>
     {props.users.map(user => {
         const blogAmount = props.blogs.filter(blog => blog.user.id === user.id).length
         return(
-        <table>
-            <tbody>
-                <tr key={user.id}>
-                    <td>
-                        <Link to={`/users/${user.id}`}> {user.name} </Link>
-                    </td>
-                    <td>
-                        {blogAmount}
-                    </td>
-                </tr>    
-            </tbody>
-        </table>
+          <tr key={user.id}>
+            <td>
+                <Link to={`/users/${user.id}`}> {user.name} </Link>
+            </td>
+            <td>
+              {blogAmount}
+            </td>
+          </tr> 
         )
     }
     )}
+    
+        </tbody>
+      </table>
     </div>
   )
 }
